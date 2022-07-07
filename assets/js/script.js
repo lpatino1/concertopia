6b3064ab0b85abe02cd3744cea959a62ea13994c
Kristina Faye <kglinoga@gmail.com>
2022-07-07 15:28:27 -0700

    got fresh Materiallize code for nav bar - there IS a mobile-collapse version that doesn't exactly match our wireframe, but does quite well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//Mobile Collapse Navbar from Materialize
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
 //Ticketmaster API
 var requestTickermaster = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=K4bW9KYnGTzMZH5cHGLHBQ6Y2l0AO1cQ';
 
