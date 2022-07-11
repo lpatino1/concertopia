90095b01a5bbe0099883710f9b48148275fb8020
Parker McKillop <mckilpar000@hotmail.com>
2022-07-10 19:26:21 -0700

    users are no longer reprompted for location if they've visited before and given their location, play history still in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,22 @@ var locateBtn = $(".locate")
 //user Location
 //success call back
 
-function locateUser() {
+if(localStorage.getItem("data") !== null){
+    var data = JSON.parse(localStorage.getItem("data"))
+    console.log(data)
+    locateBtn.parent().text(`${data.results[0].components.town}, ${data.results[0].components.state_code}`)
+    locateBtn.remove()
+}
+
+function locateUser(event) {
+    event.stopPropagation()
     navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude;
         var long = position.coords.longitude
         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=888ab51ae378491c9cc6646f56803e81`)
             .then(response => response.json())
             .then(function (data) {
-                
+
                 locateBtn.parent().text(`${data.results[0].components.town}, ${data.results[0].components.state_code}`)
                 locateBtn.remove()
                 localStorage.setItem("data", JSON.stringify(data))
@@ -53,23 +61,28 @@ var options = {
     }
 };
 
+var histArr = []
+
 //retrieves lyrics of top trending song
 fetch('https://theaudiodb.p.rapidapi.com/trending.php?country=us&type=itunes&format=singles', options)
     .then(response => response.json())
     .then(function (data) {
 
-        console.log(data)
+        
 
         $("h1").text(`${data.trending[0].strTrack}`)
         $("h2").text(`${data.trending[0].strArtist}`)
         $("img").attr("src", `${data.trending[0].strTrackThumb}`)
 
         for (i = 1; i < data.trending.length; i++) {
-            var tRow = tBody.append(`<tr>`)
-            tRow.append(`<td>${data.trending[i].strTrack}</td>`)
-            tRow.append(`<td>${data.trending[i].strArtist}</td>`)
-            tRow.append(`<td>${data.trending[i].strAlbum}</td>`)
-            tRow.append(`<td><a class="listen waves-effect waves-light btn">Play</a></td>`)
+            tBody.append(`<tr class=${i}>`)
+
+            var newTr = $(`.${i}`)
+
+            newTr.append(`<td>${data.trending[i].strTrack}</td>`)
+            newTr.append(`<td>${data.trending[i].strArtist}</td>`)
+            newTr.append(`<td>${data.trending[i].strAlbum}</td>`)
+            newTr.append(`<td><a class="listen waves-effect waves-light btn">Play</a></td>`)
         }
 
         searchName = data.trending[0].strArtist
@@ -107,30 +120,31 @@ $(".submitBtn").click(function (event) {
         .then(response => response.json())
         .then(function (data) {
             tBody.empty();
-            
+
 
             console.log(data)
 
             //edge case for no api
-            if(data.track !== null){
-            searchName = data.track[0].strArtist
-            searchSong = data.track[0].strTrack
+            if (data.track !== null) {
+                searchName = data.track[0].strArtist
+                searchSong = data.track[0].strTrack
 
 
 
-            //lyrics api
-            fetch(`https://api.lyrics.ovh/v1/${searchName}/${searchSong}`)
-                .then(response => response.json())
-                .then(function (data) {
-                    $("#songLyrics").empty()
-                    if (data.lyrics !== null) {
-                        var lyricsArr = data.lyrics.split("\n")
-                        for (i = 0; i < lyricsArr.length; i++) {
-                            $("#songLyrics").append(`<li>${lyricsArr[i]}</li>`)
+                //lyrics api
+                fetch(`https://api.lyrics.ovh/v1/${searchName}/${searchSong}`)
+                    .then(response => response.json())
+                    .then(function (data) {
+                        $("#songLyrics").empty()
+                        if (data.lyrics !== null) {
+                            var lyricsArr = data.lyrics.split("\n")
+                            for (i = 0; i < lyricsArr.length; i++) {
+                                $("#songLyrics").append(`<li>${lyricsArr[i]}</li>`)
+                            }
                         }
                     }
-                }
-                )}
+                    )
+            }
 
             if (data.track !== null) {
                 $("h1").text(`${data.track[0].strTrack}`)
@@ -143,11 +157,17 @@ $(".submitBtn").click(function (event) {
                 }
 
                 for (i = 1; i < data.track.length; i++) {
-                    var tRow = tBody.append(`<tr>`)
-                    tRow.append(`<td>${data.track[i].strTrack}</td>`)
-                    tRow.append(`<td>${data.track[i].strArtist}</td>`)
-                    tRow.append(`<td>${data.track[i].strAlbum}</td>`)
-                    tRow.append(`<td><a class="listen waves-effect waves-light btn">Play</a></td>`)
+
+                    tBody.append(`<tr class=iter${i}>`)
+
+                    var newTr = $(`.iter${i}`)
+
+                    newTr.append(`<td>${data.track[i].strTrack}</td>`)
+                    newTr.append(`<td>${data.track[i].strArtist}</td>`)
+                    newTr.append(`<td>${data.track[i].strAlbum}</td>`)
+                    newTr.append(`<td><a class="listen waves-effect waves-light btn">Play</a></td>`)
+
+
                 }
             } else {
                 $("h1").text(`Artist not found, please search for another.`)
@@ -160,27 +180,56 @@ $(".submitBtn").click(function (event) {
 
 )
 
+histArr = []
+if (localStorage.getItem("songHist")==null){
+    localStorage.setItem("songHist", JSON.stringify(histArr))
+}else{
+    $(".histTable").empty()
+    var histAppend = JSON.parse(localStorage.getItem(`songHist`))
+    for (i = 0; i < histAppend.length; i++) {
+        $(".histTable").append(`<tr class=hist${i}>`)
 
-tBody.on("click", function (event) {
-    event.stopPropagation
-    if (event.target.classList.contains("listen")){
-        event.stopPropagation()
-        console.log("working")
+        var newTr = $(`.hist${i}`)
+   
+        newTr.append(histAppend[i])
+}
+}
 
-        if(localStorage.getItem("songHist") !== null){
-        
-        
-        console.log($(event.target).parent().parent().html())
-        localStorage.setItem(`songHist`, JSON.stringify($(event.target).parent().parent().children().html()))
 
-        
-        var histAppend =  JSON.parse(localStorage.getItem(`songHist`))
-        $(".histTable").append(histAppend)
+
+tBody.on("click", $(".listen"), function (event) {
+    event.stopPropagation()
     
-        //spotify code
-    }else{
-        localStorage.setItem("songHist", JSON.stringify($(event.target).parent().parent().html()))
-}}})
+
+    if (event.target.classList.contains("listen")) {
+        
+        console.log("working")
+            
+            //I think the problem is here, getting ta help tomorrow morning
+            var histObj = JSON.parse(localStorage.getItem("songHist"))
+            histArr.push(histObj)
+            console.log(histObj)
+            histArr.unshift($(event.target).parent().parent().html())
+          
+            console.log(histArr)
+           
+            localStorage.setItem(`songHist`, JSON.stringify(histArr))
+
+
+            $(".histTable").empty()
+            var histAppend = JSON.parse(localStorage.getItem(`songHist`))
+            for (i = 0; i < histAppend.length; i++) {
+                $(".histTable").append(`<tr class=hist${i}>`)
+
+                var newTr = $(`.hist${i}`)
+               
+                newTr.append(histAppend[i])
+            
+
+            //spotify code
+
+  }
+}})
 
 // Spotify SDK URI
 /*const play = ({
